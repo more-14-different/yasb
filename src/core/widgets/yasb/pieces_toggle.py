b5b442a06fa411501286c1767ba3a096bfbca9cd
more_14_different <more.14.different@gmail.com>
2026-06-19 23:34:16 +0800

    Fix pieces toggle icon hit area

--- src/core/widgets/yasb/pieces_toggle.py
+++ src/core/widgets/yasb/pieces_toggle.py
@@ -1,8 +1,42 @@
+from collections.abc import Callable
+
 from PyQt6.QtCore import pyqtSignal, Qt
+from PyQt6.QtGui import QColor, QMouseEvent, QPainter
 from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout, QFrame, QLabel
 from core.validation.widgets.yasb.pieces_toggle import PiecesToggleConfig
 from core.widgets.base import BaseWidget
 import re
+
+
+class _ClickableLayerLabel(QLabel):
+    """Icon label with a nearly transparent painted layer for hit testing."""
+
+    def __init__(self, text: str, mouse_handler: Callable[[QMouseEvent], None]):
+        super().__init__(text)
+        self._mouse_handler = mouse_handler
+        self.setMouseTracking(True)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.fillRect(self.rect(), QColor(0, 0, 0, 1))
+        painter.end()
+        super().paintEvent(event)
+
+    def mousePressEvent(self, event: QMouseEvent):
+        if self.rect().contains(event.pos()):
+            event.accept()
+            return
+
+        super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        if self.rect().contains(event.pos()):
+            self._mouse_handler(event)
+            event.accept()
+            return
+
+        super().mouseReleaseEvent(event)
+
 
 class PiecesToggleWidget(BaseWidget):
     validation_schema = PiecesToggleConfig
@@ -34,10 +68,12 @@ class PiecesToggleWidget(BaseWidget):
         
         self.register_callback("toggle_pieces", self._toggle_pieces)
         
-        # We handle clicking via mouseReleaseEvent based on Y coordinate
+        # The icon labels handle mouse release events themselves. This avoids the
+        # outer capsule-shaped container being clickable outside the icon layer.
         self.callback_left = self.config.callbacks.on_left
         self.callback_right = self.config.callbacks.on_right
         self.callback_middle = self.config.callbacks.on_middle
+        self.mouseReleaseEvent = self._ignore_container_mouse_release
         
         self._event_service.register_event("pieces_widget_state_changed", self._state_changed_signal)
         self._state_changed_signal.connect(self._on_state_changed)
@@ -45,7 +81,11 @@ class PiecesToggleWidget(BaseWidget):
         self._update_labels()
         
     def _build_two_toggles(self):
-        def process_content(content: str, is_alt: bool = False) -> list[QLabel]:
+        def process_content(
+            content: str,
+            mouse_handler: Callable[[QMouseEvent], None],
+            is_alt: bool = False,
+        ) -> list[QLabel]:
             label_parts = re.split(r"(<span.*?>.*?</span>)", content)
             label_parts = [part for part in label_parts if part]
             widgets: list[QLabel] = []
@@ -58,10 +98,10 @@ class PiecesToggleWidget(BaseWidget):
                     class_name = re.search(r'class=(["\'])([^"\']+?)\1', part)
                     class_result = class_name.group(2) if class_name else "icon"
                     icon = re.sub(r"<span.*?>|</span>", "", part).strip()
-                    label = QLabel(icon)
+                    label = _ClickableLayerLabel(icon, mouse_handler)
                     label.setProperty("class", class_result)
                 else:
-                    label = QLabel(part)
+                    label = _ClickableLayerLabel(part, mouse_handler)
                     label.setProperty("class", "label alt" if is_alt else "label")
                 
                 label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -78,8 +118,8 @@ class PiecesToggleWidget(BaseWidget):
         top_layout.setSpacing(0)
         top_layout.setContentsMargins(0, 0, -4, 0)
         top_layout.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
-        self._time_widgets = process_content(self.config.label, False)
-        self._time_widgets_alt = process_content(self.config.label_alt, True)
+        self._time_widgets = process_content(self.config.label, self._handle_time_mouse_event)
+        self._time_widgets_alt = process_content(self.config.label_alt, self._handle_time_mouse_event, True)
         for w in self._time_widgets + self._time_widgets_alt:
             top_layout.addWidget(w)
         self._widget_container_layout.addWidget(top_frame, alignment=Qt.AlignmentFlag.AlignTop)
@@ -90,24 +130,31 @@ class PiecesToggleWidget(BaseWidget):
         bottom_layout.setSpacing(0)
         bottom_layout.setContentsMargins(0, 0, -4, 0)
         bottom_layout.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
-        self._pieces_widgets = process_content(self.config.label, False)
-        self._pieces_widgets_alt = process_content(self.config.label_alt, True)
+        self._pieces_widgets = process_content(self.config.label, self._handle_pieces_mouse_event)
+        self._pieces_widgets_alt = process_content(self.config.label_alt, self._handle_pieces_mouse_event, True)
         for w in self._pieces_widgets + self._pieces_widgets_alt:
             bottom_layout.addWidget(w)
         self._widget_container_layout.addWidget(bottom_frame, alignment=Qt.AlignmentFlag.AlignBottom)
 
-    def _handle_mouse_events(self, event):
+    def _ignore_container_mouse_release(self, event: QMouseEvent):
+        event.ignore()
+
+    def _handle_time_mouse_event(self, event: QMouseEvent):
         if event.button() == Qt.MouseButton.LeftButton:
-            # Top half for time source toggle, bottom half for pieces toggle
-            if event.pos().y() < self.height() / 2:
-                self._toggle_time_source()
-            else:
-                self._run_callback(self.callback_left)
+            self._toggle_time_source()
         elif event.button() == Qt.MouseButton.MiddleButton:
             self._run_callback(self.callback_middle)
         elif event.button() == Qt.MouseButton.RightButton:
             self._run_callback(self.callback_right)
-                
+
+    def _handle_pieces_mouse_event(self, event: QMouseEvent):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self._run_callback(self.callback_left)
+        elif event.button() == Qt.MouseButton.MiddleButton:
+            self._run_callback(self.callback_middle)
+        elif event.button() == Qt.MouseButton.RightButton:
+            self._run_callback(self.callback_right)
+
     def _toggle_time_source(self):
         self._time_is_on = not self._time_is_on
         self._event_service.emit_event("pieces_time_source_changed", self._time_is_on, self.screen_name)
